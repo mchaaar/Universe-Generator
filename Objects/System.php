e50eae54e60f4b398a979023fb2395724472b882
Martin Wenzel <wenzel.martin.de@gmail.com>
2022-12-11 00:49:08 +0100

    Artificial Body Generator and Object

--- Objects/System.php
+++ Objects/System.php
@@ -5,6 +5,7 @@ class System {
     public string $name;
     public $planets = [];
     public $asteroids = [];
+    public $artificialBodies = [];
 
     public function __construct(string $name) {
         $this->name = $name;
